23fc420423ad2693c6cfe9927603ac282ae156d3
Konstantin Kosolapov <kkosolapov@gmail.com>
2022-06-18 17:16:04 +0300

    Update main_view_controller.swift

--- View/main_view_controller.swift
+++ View/main_view_controller.swift
@@ -1,10 +1,24 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    let network = DataBase(api: "zcofu8bnd3ldl")
+    var dataSourse: Sheet = [] {
+        didSet {  print(dataSourse) }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        network.obtainSheet { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let sheet):
+                    self.dataSourse = sheet.getMainLayerOfSheet()
+                case .failure(let error):
+                    self.someWrongAlert(self, "Atension", error.localizedDescription)
+                }
+            }
+        }
     }
-
+    
 }
