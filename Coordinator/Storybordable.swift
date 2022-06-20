0d6cce2d405820e26c0fe3a3ef2c3f29493df1ca
Konstantin Kosolapov <kkosolapov@gmail.com>
2022-06-20 08:31:02 +0300

    Create Storybordable.swift

---
+++ Coordinator/Storybordable.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+protocol Storybordable {
+    static func createObject() -> Self
+}
+
+extension Storybordable where Self: UIViewController {
+    static func createObject() -> Self {
+        let id = String(describing: self)
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        return storyboard.instantiateViewController(withIdentifier: id) as! Self
+    }
+}
