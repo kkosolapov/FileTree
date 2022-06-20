e35c9016805cdb9fe2c1304f330f1553c49070b2
Konstantin Kosolapov <kkosolapov@gmail.com>
2022-06-20 12:25:39 +0300

    Update Sheet.swift

--- Database/Models/Sheet.swift
+++ Database/Models/Sheet.swift
@@ -6,10 +6,13 @@ extension Sheet {
     
     func getMainLayerOfSheet() -> Sheet {
         return self.filter { $0.parentId == "" }
-            .sorted { $0.type == .d || $1.type != .d }
+                   .sorted { $0.type == .d || $1.type != .d }
     }
     
     func getChildItems(by id: String) -> Sheet {
         return self.filter { $0.parentId == id }
+    }
+    func getSortedSheet() -> Sheet {
+        return self.sorted { $0.type == .d || $1.type != .d }
     }
 }
