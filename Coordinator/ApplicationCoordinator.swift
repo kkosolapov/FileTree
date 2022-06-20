5aa7fc49f864be4fe273bea545ec369d91ad049a
Konstantin Kosolapov <kkosolapov@gmail.com>
2022-06-20 12:22:22 +0300

    Update ApplicationCoordinator.swift

--- Coordinator/ApplicationCoordinator.swift
+++ Coordinator/ApplicationCoordinator.swift
@@ -6,27 +6,27 @@ class AppCoordinator: Coordinator {
     
     init(_ navigationController: UINavigationController) {
         self.navigationController = navigationController
+        self.navigationController.navigationBar.prefersLargeTitles = true
     }
     
     func start() {
         let vc = MainViewController.createObject()
         vc.coordinator = self
         self.navigationController.pushViewController(vc, animated: true)
-        
     }
   
     func goToDetail(_ item: SheetItem) {
         let vc = DetailViewController.createObject()
         vc.coordinator = self
         vc.item = item
-        vc.title = item.content
         navigationController.pushViewController(vc, animated: true)
     }
     
-    func goToMainVC(_ datasourse: Sheet , _ dataBase: Sheet) {
+    func goToMainVC(title: String,datasourse: Sheet ,dataBase: Sheet) {
         let vc = MainViewController.createObject()
         vc.coordinator = self
-        vc.dataSourse = datasourse
+        vc.title = String(title.split(separator: ".").first ?? "File")
+        vc.dataSourse = datasourse.getSortedSheet()
         vc.dataBace = dataBase
         self.navigationController.pushViewController(vc, animated: true)
     }
